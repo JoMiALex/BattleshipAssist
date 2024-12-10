e85e68f2c9ef56a10c467a12ce7e63724c9b53f4
JoMiAlex <lottjohnmichael@yahoo.com>
2024-12-09 17:01:28 -0800

    updates before stepping out

--- AttackBot.py
+++ AttackBot.py
@@ -9,13 +9,14 @@ class AttackBot:
         self.targets = []
 
     def attack(self, board):
-        
-        if self.targets:
-            x,y = self.targets.pop()
-        else:
-            while True:
-                x = random.randint(0, 9)
-                y = random.randint(0, 9)
-                if (x,y) not in self.attackLog:
-                    break
-        
+        if not board.isGameOver():
+            if self.targets:
+                x,y = self.targets.pop()
+            else:
+                while True:
+                    x = random.randint(0, 9)
+                    y = random.randint(0, 9)
+                    if (x,y) not in self.attackLog:
+                        break
+            self.attackLog.add((x, y))
+            
